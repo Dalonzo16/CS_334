140470d40d79e8b5162747ec61b665e6055b01b9
Dalonzo16 <143286079+Dalonzo16@users.noreply.github.com>
2025-05-05 10:08:38 -0600

    Update shop.js

--- Part 4/scripts/shop.js
+++ Part 4/scripts/shop.js
@@ -34,7 +34,7 @@ async function displayProducts() {
                 return false;
 
             // Match selected category (or "all")
-            const matchesCategory = selectedCategory === "all" || product.category === selectedCategory.toLowerCase();
+            const matchesCategory = selectedCategory === "all" || product.category === selectedCategory;
 
             // Match selected price range
             let matchesPrice = true;
